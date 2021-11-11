e02e8e5e2dcd53ffbb8acbbb34b0e29a9aceaebe
gesora96 <nahidahmed9933@gmail.com>
2021-11-11 19:02:01 +0600

    display user from my api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Total User: {users.length}</h1>
+      <ul>
+        {
+          // map users
+          users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
